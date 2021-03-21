cf8c695ce64a183225adf59271a0236e30caeffd
mehedi-hasan-anik <anikm2283@gmail.com>
2021-03-21 23:23:48 +0600

    map

--- src/Components/Destination/Destination.js
+++ src/Components/Destination/Destination.js
@@ -52,7 +52,17 @@ const Destination = () => {
                              </div>
                        </div>
                        <div className="col-sm-12 col-md-12 col-lg-8 map-area">
-                              <img src={map} alt=""/>
+                              {/* <img src={map} alt=""/> */}
+                              <iframe
+                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.521260322283!2d106.8195613507864!3d-6.194741395493371!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f5390917b759%3A0x6b45e67356080477!2sPT%20Kulkul%20Teknologi%20Internasional!5e0!3m2!1sen!2sid!4v1601138221085!5m2!1sen!2sid"
+                                    width="100%"
+                                    height="450"
+                                    frameBorder="0"
+                                    style={{ border: 0 }}
+                                    allowFullScreen=""
+                                    aria-hidden="false"
+                                    tabIndex="0"
+                                    />
                        </div>
                  </div>
             </div>
